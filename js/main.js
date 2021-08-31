c9afc0e1e5d7d0be210886b6446c6b7fbdd1f739
Alex Harris <alexharris@Alexs-Mac-mini.attlocal.net>
2021-08-31 00:10:29 -0700

    stuff

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $('.painting-slider').slick({
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToShow: 1,
+        // adaptiveHeight: true
+        // adaptiveHeight: true,
+        // dots: true
+        // responsive: [
+        //   {
+        //     breakpoint: 768,
+        //     settings: {
+        //       arrows: false,
+        //       centerMode: true,
+        //       centerPadding: '40px',
+        //       slidesToShow: 1
+        //     }
+        //   },
+        //   {
+        //     breakpoint: 480,
+        //     settings: {
+        //       arrows: false,
+        //       centerMode: true,
+        //       centerPadding: '40px',
+        //       slidesToShow: 1
+        //     }
+        //   }
+        // ]
+      });
+               
+  });
+          
